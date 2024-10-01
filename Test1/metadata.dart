5df62842ead3d5f6f1a38156d05f53f8d4443fca
mahimanni <mannimahima@gmail.com>
2024-10-01 05:46:08 +0530

    metadata

---
+++ Test1/metadata.dart
@@ -0,0 +1,20 @@
+/*
+class Human{
+    //@overriden
+    void run(){
+        print("Human is running");
+    }
+}
+class Man extends Human{
+    //@Overriding 
+    @override
+    void run(){
+        print("Man is running");
+    }
+}
+void main(){
+    Man m= new Man();
+    m.run();
+}
+*/
+
